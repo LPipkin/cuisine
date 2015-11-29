0bf4dd246f6340556a50c1a8baa1d42d7ba15a22
Louis Pipkin <louis@louispipkin.com>
2015-11-29 10:43:39 -0500

    add works with unkown diet

--- __init__.py
+++ __init__.py
@@ -27,7 +27,7 @@ def home():
 @app.route('/search', methods=['GET', 'POST'])
 def search():
     #return '<h1>Hello, Search!!!</h1>'
-    return render_template('search.html')
+    return render_template('search.html', methods=['GET', 'POST'])
 
 @app.route('/search/results', methods=['GET', 'POST'])
 def searchResults():
@@ -63,8 +63,42 @@ def edit(rec_id):
 
 @app.route('/add', methods=['GET', 'POST'])
 def add():
-    #
     return render_template('add_recipes.html')
+
+@app.route('/submit', methods=['GET', 'POST'])
+def submit():
+    if request.method == "GET":
+        if request.args['sub'] == "submit":
+            nam = request.args['name']
+            die = request.args["diet"]
+            des = request.args["desc"]
+            ing = request.args["ingr"]
+            ins = request.args["inst"]
+            db = MySQLdb.Connection(host="localhost", user="root", db="cuisineRecipes",
+                cursorclass=MySQLdb.cursors.DictCursor)
+            cursor = db.cursor()
+            # check if diet exists
+            cursor.execute("""SELECT * FROM Diets WHERE name='{0}'""".format(die))
+            rv = cursor.fetchone()
+            if rv is None:
+                cursor.execute("""INSERT INTO Diets(name) VALUES('{0}')""".format(die))
+                db.commit()
+            # insertion
+            cursor.execute("""INSERT INTO 
+                Recipes(name, diet, description, ingredients, instructions) 
+                VALUES('{0}', '{1}', '{2}', '{3}', '{4}')"""
+                .format(nam, die, des, ing, ins))
+            db.commit()
+            # go to view of new entry
+            cursor.execute("""SELECT * FROM Recipes WHERE instructions='{0}'""".format(ins))
+            rv = cursor.fetchone()
+            rec_id = rv["recipeID"]
+            db.close()
+            return render_template('view_recipes.html', rec=rec_id, rv=rv)
+        else:
+            return render_template('index.html')
+    else:
+        return render_template('search.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
